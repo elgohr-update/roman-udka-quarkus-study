2af5345657076ecec8c56215a84888f61688e86a
Roman Udka <udka.roman@gmail.com>
2021-03-05 03:02:09 +0200

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -1,32 +1,41 @@
 from flask import Flask, request, jsonify
-import uuid
 
-default_data = {"2": "{name:'2'}", "3": "{name:'3'}"}
+default_data = {}
+all_data = []
 app = Flask(__name__)
 
 
 @app.route('/received_email', methods=['POST'])
 def generate_auth_device_key():
-    req_data = request.get_json(silent=True)
-    id = str(uuid.uuid4())
-    default_data[id] = req_data
+    html = request.form["html"]
+    temp_pass = html.split("</strong>")[1].split("<br>")[0].strip()
+    to = request.form["to"]
+    uid = to.split("<")[1].split("@")[0]
+    default_data[uid] = temp_pass
+    all_data.append(request.form)
     return "ok"
 
 
 @app.route('/', methods=['GET'])
 def app_index():
-    str = "<h2><font color=\"green\">emails API.</font></h2>" \
-          "<p>/received_email POST</p>" \
-          "<p>/get_email GET all</p>" \
-          "<p>/get_email/<uuid> GET by id</p>" \
-          "<p>/delete_email DELETE</p>"
+    str_ = "<h2><font color=\"green\">emails API.</font></h2>" \
+           "<p>/received_email POST</p>" \
+           "<p>/get_email GET all</p>" \
+           "<p>/get_email/<uuid> GET by id</p>" \
+           "<p>/get_all_data GET all incoming data</p>" \
+           "<p>/delete_email DELETE</p>"
 
-    return str
+    return str_
 
 
 @app.route('/get_email', methods=['GET'])
 def get_all_email():
     return jsonify(default_data)
+
+
+@app.route('/get_all_data', methods=['GET'])
+def get_all_email():
+    return jsonify(all_data)
 
 
 @app.route('/get_email/<uuid>', methods=['GET'])
